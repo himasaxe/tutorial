673a4e7ae6818ce36365af6f16ba11b024e19dd0
devendar <devendar.malothu@gmail.com>
2024-05-08 14:03:32 +0530

    first commit

---
+++ src/test/java/com/demo/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		
+		System.out.println("open new world");
+	}
+}
---
+++ src/test/java/com/demo/one.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+
+public class one {
+
+	
+
+	@Test
+	public void first() {
+		System.out.println("one two");
+	}
+}
